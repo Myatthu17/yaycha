a398d1eecae2b6fb7881367f852a3b53c74d36aa
Myat Thu Khaing <myatthu10302@gmail.com>
2025-09-24 16:05:11 +0900

    Separate Components (Item, List)

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,6 @@
-function Item({ content, name }) {
-  return (
-    <li style={{ padding: 10, borderBottom: "1px solid #ddd"}}>
-      {content} -<b>{name}</b>
-    </li>
-  );
-}
+import Item from "./Item.jsx";
+import List from "./List.jsx";
 
-function List({ children }) {
-  return (
-    <ul
-    style={{
-      listStyle: "none",
-      padding: 0,
-      margin: 0,
-      border: "1px solid #ddd",
-      borderRadius: 8,
-      overflow: "hidden",
-    }}>
-      {children}
-    </ul>
-  );
-}
 
 export default function App() {
   return (
