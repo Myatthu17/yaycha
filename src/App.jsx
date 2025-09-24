f2005a86290b1ad00beb27302861153eb43a3c15
Myat Thu Khaing <myatthu10302@gmail.com>
2025-09-24 23:47:24 +0900

    Loop content with map function + useState function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 import Item from "./Item.jsx";
 import List from "./List.jsx";
 
 
 export default function App() {
+
+  const [data, setData] = useState([
+    { id: 1, content: "Hello, World!", name: "Alice" },
+    { id: 2, content: "React is awesome!", name: "Bob" },
+    { id: 3, content: "Let's build something cool.", name: "Charlie" },
+  ])
+
   return (
     <div>
       <h1>Yaycha</h1>
       <List>
-        <Item content="Hello, World!" name="Alice" />
-        <Item content="React is fun." name="Bob" />
+        {data.map(item => (
+          <Item content = {item.content} name = {item.name} />
+        ))}
       </List>
     </div>
   );
