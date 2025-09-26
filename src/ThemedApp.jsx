5b1f3c385128f0957e74e118e18d4ae9f6f712f6
Myat Thu Khaing <myatthu10302@gmail.com>
2025-09-27 04:13:13 +0900

    Add Toggle Dark/Light Mode using MUI

--- src/ThemedApp.jsx
+++ src/ThemedApp.jsx
@@ -1,15 +1,8 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useMemo } from "react";
 
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 import App from "./App.jsx";
-
-
-const theme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
-});
 
 const AppContext = createContext();
 export function useApp() {
@@ -18,10 +11,17 @@ export function useApp() {
 
 export default function ThemedApp() {
     const [showForm, setShowForm] = useState(false);
+    const [mode, setMode] = useState('dark');
+
+    const theme = useMemo(() => {
+        return createTheme({
+        palette: { mode },
+    });
+    }, [ mode ]);
 
     return (
         <ThemeProvider theme={theme}> 
-            <AppContext.Provider value={{ showForm, setShowForm }}>
+            <AppContext.Provider value={{ showForm, setShowForm, mode, setMode }}>
                 <App />
                 <CssBaseline />
             </AppContext.Provider>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,10 +12,11 @@ import {
     Menu as MenuIcon,
     Add as AddIcon,
     LightMode as LightModeIcon,
+    DarkMode as DarkModeIcon,
 } from "@mui/icons-material";
 
 export default function Header() {
-    const { showForm, setShowForm } = useApp();
+    const { showForm, setShowForm, mode, setMode } = useApp();
 
     return (
         <AppBar position="static">
@@ -35,12 +36,21 @@ export default function Header() {
                         onClick={()=> setShowForm(!showForm)}>
                             <AddIcon/>
                     </IconButton>
-
-                    <IconButton
-                        color="inherit"
-                        edge="end">
+                    { mode == 'dark' ? 
+                        (<IconButton
+                            color="inherit"
+                            edge="end"
+                            onClick={() => setMode("light")}>
                             <LightModeIcon/>
-                    </IconButton>
+                        </IconButton>) :
+
+                        (<IconButton
+                            color="inherit"
+                            edge="end"
+                            onClick={() => setMode("dark")}>
+                            <DarkModeIcon/>
+                        </IconButton>)
+                    }      
 
                 </Box>
 
